d2a16d8bf8132fdc89bc06e8beda6091a81f975a
santiago alarcon <santiagoalarcon2@hotmail.com>
2019-12-02 19:43:41 -0300

    asas

---
+++ Unity-and-NodejsExpress/Assets/ClientAPI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class ClientAPI : MonoBehaviour {
+
+    public string url;
+	 void Start()
+    {
+        StartCoroutine(Get(url));
+    }
+    public IEnumerator Get(string url)
+	{
+		using(UnityWebRequest www = UnityWebRequest.Get(url))
+		{
+            yield return www.Send();
+
+			if (www.isError)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				if (www.isDone)
+				{
+					// handle the result
+					var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+					Debug.Log(result);
+				}
+				else
+				{
+					//handle the problem
+					Debug.Log("Error! data couldn't get.");
+				}
+
+			}
+		}
+	}
+}
